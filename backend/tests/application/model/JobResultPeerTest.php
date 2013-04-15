c577b7254a0166201b915e69cefad982dcaa9821
Christoph Lukas <lukas@pingworks.de>
2013-04-15 11:49:53 +0200

    * copyright and licence

--- backend/tests/application/model/JobResultPeerTest.php
+++ backend/tests/application/model/JobResultPeerTest.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ * Copyright (C) 2013 pingworks - Alexander Birk und Christoph Lukas
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 class Application_Model_JobResultPeerTest extends PHPUnit_Framework_TestCase
 {
 
@@ -22,7 +38,7 @@ class Application_Model_JobResultPeerTest extends PHPUnit_Framework_TestCase
 		$this->assertAttributeEquals(1, 'total', $data[0]);
 		$this->assertAttributeEquals(0, 'skipped', $data[0]);
 		$this->assertAttributeEquals(0, 'failed', $data[0]);
-		
+
 		$this->assertType('Application_Model_JobResult', $data[1]);
 		$this->assertAttributeEquals('test', 'name', $data[1]);
 		$this->assertAttributeEquals('url', 'url', $data[1]);
@@ -30,7 +46,7 @@ class Application_Model_JobResultPeerTest extends PHPUnit_Framework_TestCase
 		$this->assertAttributeEquals(2, 'total', $data[1]);
 		$this->assertAttributeEquals(4, 'skipped', $data[1]);
 		$this->assertAttributeEquals(2, 'failed', $data[1]);
-		
+
 	}
 
 }
