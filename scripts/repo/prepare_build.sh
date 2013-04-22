a41c3f194dd3c6ae653ea34a13e39fe9eb5f476c
Christoph Lukas <lukas@pingworks.de>
2013-04-22 23:29:05 +0200

    * use property file to pass build parameters to jobs

---
+++ scripts/repo/prepare_build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SCRIPTDIR=$(dirname $0)
+. $SCRIPTDIR/../configs/repo.conf
+. $SCRIPTDIR/../configs/vcs.conf
+. $SCRIPTDIR/../shared/common.sh
+. $SCRIPTDIR/../shared/$VCS.sh
+
+BRANCH=$1
+REV=$2
+BUILDNR=$3
+
+if [ -z "$BRANCH" -o -z "$REV" -o -z "$BUILDNR" ]; then
+  echo "Usage: $0 <branch> <revision> <build_id>"
+  exit 1
+fi
+set -e
+validateBranch $BRANCH
+validateRev $REV
+validateBuildNr $BUILDNR
+createDirIfNotExists $JENKINS_PROPERTYFILE_DIR
+
+getBundleName $BRANCH $REV $BUILDNR
+getBranchNr $BRANCH
+getBranchUrl $BRANCH
+bash ${SCRIPTDIR}/../repo/prepare_bundle.sh $BRANCH $REV $BUILDNR
+
+cat << EOF > $JENKINS_PROPERTYFILE_DIR/${BUNDLE}.properties
+BUILD_DATE=$(date +%Y%m%d)
+BRANCH=${BRANCH}
+BRANCH_NUM=${BRANCHNR}
+BRANCH_URL=${BRANCHURL}
+REV=${REV}
+BNUM=${BUILDNR}
+VERSION="${BRANCHNR}+${VCS}${BUILD_DATE}.${REV}-${BUILDNR}"
+BUNDLE=${BUNDLE}
+EOF
+
+cp $JENKINS_PROPERTYFILE_DIR/${BUNDLE}.properties $JENKINS_PROPERTYFILE_DIR/${BUILDNR}.properties
+
+bash ${SCRIPTDIR}/../repo/add_metadata.sh ${BUNDLE} status first_stage_in_progress
