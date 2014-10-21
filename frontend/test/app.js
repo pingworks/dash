d35d19f489e5d1bc8e04147d939665102a933fac
Lars-Erik Kimmel <lkimmel@seibert-media.net>
2014-10-21 17:01:35 +0200

    AREG-21: App fuer Jasmin-Tests aktualisiert

--- frontend/test/app.js
+++ frontend/test/app.js
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013 pingworks - Alexander Birk und Christoph Lukas
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -10,46 +10,58 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 Ext.onReady(function() {
     Ext.application({
 	    name: 'Dash',
-	    models:[
-	        'Branch',
-	        'Bundle',
-	        'Change',
-	        'JobResult',
-	        'JobStatus',
-	        'StageStatus'
-	    ],
-	
-	    views: [
-	        'BundleGrid',
-	        'ChangeToolTip',
-	        'JobResultToolTip',
-	        'StoreMenu',
-	        'ToolTip',
-	        'TopToolbar',
-	        'Viewport'
-	    ],
-	
-	    stores: [
-	        'Branches',
-	        'Bundles',
-	        'Changes',
-	        'JobResults',
-	        'JobStatus',
-	        'StageStatus'
-	    ],
-	
-	    controllers: [
-	        'Bundle',
-	        'Change',
-	        'JobResult'
-	    ],
+        models:[
+            'Branch',
+            'Bundle',
+            'Content',
+            'Change',
+            'JobResult',
+            'JobStatus',
+            'StageStatus',
+            'Environment',
+            'LockDuration'
+        ],
+
+        views: [
+            'BundleGrid',
+            'ChangeToolTip',
+            'JobResultToolTip',
+            'ToolTip',
+            'TopToolbar',
+            'BottomToolbar',
+            'DeploymentWindow',
+            'EnvironmentsWindow',
+            'EnvironmentGrid',
+            'Viewport'
+        ],
+
+        stores: [
+            'Branches',
+            'Bundles',
+            'Contents',
+            'Changes',
+            'JobResults',
+            'JobStatus',
+            'StageStatus',
+            'Environments',
+            'LockDurations'
+        ],
+
+        controllers: [
+            'Bundle',
+            'Branch',
+            'Change',
+            'JobResult',
+            'Deployment',
+            'Environment'
+        ],
 
         launch: function() {
             //include the tests in the test.html head
