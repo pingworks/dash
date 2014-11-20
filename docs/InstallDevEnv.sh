9a440c62d23d75f93c753aa9545314502878608c
Christoph Lukas <lukas@pingworks.de>
2014-11-20 13:50:52 +0100

    fixed install script for debian wheezy

--- docs/InstallDevEnv.sh
+++ docs/InstallDevEnv.sh
@@ -1,12 +1,26 @@
-Installationsscript
--------------------
-apt-get update && apt-get upgrade -y
-apt-get install -y vim less git wget zip unzip
+#!/bin/bash
 
-# Sourcen
+##########################################################
+#Installationsscript (tested on Debian Wheezy)
+##########################################################
+
+SCMDVERSION=4.0.4.84
+PHANTOMVERSION=1.9.0
+SELENIUMURL=http://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar
+
+apt-get update && apt-get upgrade -y
+apt-get install -y vim less git wget zip unzip default-jre ant ant-contrib \
+  ruby rubygems ruby-compass php5 php5-curl zendframework xvfb iceweasel phpunit phpunit-selenium
+
+# Sources
 mkdir -p /opt/app
 cd /opt/app
 git clone https://github.com/pingworks/dash.git dash
+
+# Data
+cd /tmp/
+wget https://dash.pingworks.net/repo/master/1.1c12bb9b.105/artifacts/dash-tests-data_1+git.1c12bb9b-105_all.deb
+dpkg -i dash-tests-data_*_all.deb
 
 # /etc/hosts
 IP=$(ip addr list eth0|grep 'inet '|cut -f6 -d' '|cut -f1 -d'/')
@@ -15,8 +29,6 @@ $IP dash dash-test dash-dev
 EOF
 
 # Apache
-apt-get install -y php5 php5-curl
-
 cat << EOF > /etc/apache2/sites-available/dash-dev
 <VirtualHost *:80>
   ServerName dash-dev
@@ -81,16 +93,52 @@ a2ensite dash-test
 /etc/init.d/apache2 restart
 
 # Build Tools
-apt-get install -y default-jre ant ant-contrib ruby rubygems
 cd /usr/share/ant/lib/
 wget http://repo1.maven.org/maven2/org/vafer/jdeb/1.0.1/jdeb-1.0.1.jar
 cd /tmp/
-wget http://cdn.sencha.com/cmd/3.1.0.256/SenchaCmd-3.1.0.256-linux-x64.run.zip
-unzip SenchaCmd-3.1.0.256-linux-x64.run.zip
-chmod 755 SenchaCmd-3.1.0.256-linux-x64.run
-./SenchaCmd-3.1.0.256-linux-x64.run --prefix /opt
-rm SenchaCmd-3.1.0.256-linux-x64.run
-. ~/.bashrc
+wget http://cdn.sencha.com/cmd/${SCMDVERSION}/SenchaCmd-${SCMDVERSION}-linux-x64.run.zip
+unzip SenchaCmd-${SCMDVERSION}-linux-x64.run.zip
+chmod 755 SenchaCmd-${SCMDVERSION}-linux-x64.run
+./SenchaCmd-${SCMDVERSION}-linux-x64.run --prefix /opt
+rm SenchaCmd-${SCMDVERSION}-linux-x64.run
+
+# Tests
+# phantomjs
+cd /tmp/
+wget https://phantomjs.googlecode.com/files/phantomjs-${PHANTOMVERSION}-linux-x86_64.tar.bz2
+cd /opt
+tar xvfj /tmp/phantomjs-${PHANTOMVERSION}-linux-x86_64.tar.bz2
+rm /tmp/phantomjs-${PHANTOMVERSION}-linux-x86_64.tar.bz2
+
+# headless-selenium
+cd /opt
+git clone https://github.com/generalredneck/headless-selenium
+mv headless-selenium/headless-selenium /etc/init.d/
+mkdir -p /usr/lib/headless-selenium
+cd /usr/lib/headless-selenium
+ln -s /opt/app/dash/tests/system/selenium/user-extensions.js .
+wget ${SELENIUMURL}
+cd /opt
+mkdir -p /etc/headless-selenium
+mv headless-selenium/selenium.conf /etc/headless-selenium/
+sed -i -e "s;^#SELENIUM_JAR=.*$;SELENIUM_JAR=\$HEADLESS_SELENIUM_DIR/selenium-server-standalone-${SELENIUMVERSION}.jar;" \
+	-e 's;^#SELENIUM_OPTIONS=.*$;SELENIUM_OPTIONS="-userExtensions $HEADLESS_SELENIUM_DIR/user-extensions.js";' \
+	/etc/headless-selenium/selenium.conf 
+mkdir -p /usr/lib/headless-selenium/profiles/firefox
+cd /usr/lib/headless-selenium/profiles/firefox
+tar xvfz /opt/headless-selenium/selenium-profile.tar.gz
+#/etc/init.d/headless-selenium start
+
+
+read -p "Which user should be created to modify the source files and run the build? " username
+adduser --disabled-password --gecos "" $username
+
+grep Sencha ~/.bashrc >> /home/${username}/.bashrc
+chown -R ${username}:${username} /opt/app/dash
+chown -R ${username}:${username} /opt/Sencha
+
+su - ${username}
+# run the rest as user ${username}
 
 # Frontend build
 cd /opt/app/dash/frontend
@@ -99,46 +147,8 @@ ant build
 # Backend
 cd /opt/app/dash/backend
 echo "repoDir = /opt/dash/tests/data" >> application/configs/application.ini
-apt-get install -y zendframework
-
-# Data
-cd /tmp/
-wget https://dash.pingworks.net/repo/master/1.1c12bb9b.105/artifacts/dash-tests-data_1+git.1c12bb9b-105_all.deb
-dpkg -i dash-tests-data_*_all.deb
 
 ################################
 # You should now be able to open
-# http://dash in your Browser
+# http://dash-dev/ in your Browser
 ################################
-
-# Tests
-# phantomjs
-cd /tmp/
-wget https://phantomjs.googlecode.com/files/phantomjs-1.9.0-linux-x86_64.tar.bz2
-cd /opt
-tar xvfj /tmp/phantomjs-1.9.0-linux-x86_64.tar.bz2
-rm /tmp/phantomjs-1.9.0-linux-x86_64.tar.bz2
-
-# headless-selenium
-apt-get install -y xvfb iceweasel
-cd /opt
-git clone https://github.com/generalredneck/headless-selenium
-mv headless-selenium/headless-selenium /etc/init.d/
-mkdir -p /usr/lib/headless-selenium
-cd /usr/lib/headless-selenium
-ln -s /opt/app/dash/tests/system/selenium/user-extensions.js .
-wget http://selenium.googlecode.com/files/selenium-server-standalone-2.32.0.jar
-cd /opt
-mkdir -p /etc/headless-selenium
-mv headless-selenium/selenium.conf /etc/headless-selenium/
-sed -i -e 's;^#SELENIUM_JAR=.*$;SELENIUM_JAR=$HEADLESS_SELENIUM_DIR/selenium-server-standalone-2.32.0.jar;' \
-	-e 's;^#SELENIUM_OPTIONS=.*$;SELENIUM_OPTIONS="-userExtensions $HEADLESS_SELENIUM_DIR/user-extensions.js";' \
-	/etc/headless-selenium/selenium.conf 
-mkdir -p /usr/lib/headless-selenium/profiles/firefox
-cd /usr/lib/headless-selenium/profiles/firefox
-tar xvfz /opt/headless-selenium/selenium-profile.tar.gz
-/etc/init.d/headless-selenium start
-
-# phpunit
-apt-get install -y phpunit
-
