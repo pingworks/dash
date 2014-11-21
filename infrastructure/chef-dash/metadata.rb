be58b1cb26e604b2b561f650da916799cbee46ad
Alexander Birk <alexander.birk@hybris.com>
2014-11-21 11:35:04 +0100

    chef cookbook inital import

---
+++ infrastructure/chef-dash/metadata.rb
@@ -0,0 +1,8 @@
+name             'chef-dash'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef-dash'
+long_description 'Installs/Configures chef-dash'
+version          '0.1.0'
+
---
+++ infrastructure/chef-dash/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: chef-dash
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
