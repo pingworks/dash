72128f658663ea42eec947121bdd4b62fb4d7047
Alexander Birk <birk@pingworks.de>
2015-12-23 08:27:21 +0100

    wrong place

--- frontend/app/controller/Deployment.js
+++ frontend/app/controller/Deployment.js
@@ -88,6 +88,10 @@ Ext.define('Dash.controller.Deployment', {
         }
         var environment = this.getEnvironmentsStore().findRecord('id', values.environment);
         environment.set('locked', true);
+        console.log('values.lock=' + values.lock);
+        if (!values.lock) {
+          values.lock = 87600;
+        }
         var now = new Date();
         var lockUntil = new Date(now.getTime() + (values.lock * 3600 * 1000));
         var dateFormat = Dash.config.environment.dateformat;
